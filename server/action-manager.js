b0fc0d13934642c850e37dafbf7c7ef3f41205e2
lukakralj <luka.kralj2@gmail.com>
2019-08-17 16:27:37 +0200

    Finish action dispatch implementation

--- server/action-manager.js
+++ server/action-manager.js
@@ -14,8 +14,11 @@ const exec = require('child_process').exec;
 
 let mouseSpeeds = [1,3,6,10];
 let curSpeedInd = 1; // speed 3
-let isMouseDown = false;
+//let isMouseDown = false;
 let isWindowSwitcherOn = false;
+const indexF_mask = 0b001;
+const middleF_mask = 0b010;
+const ringF_mask = 0b100;
 
 /**
  * 
@@ -23,44 +26,111 @@ let isWindowSwitcherOn = false;
  */
 function dispatchAction(actionId) {
     console.log("received: " + actionId);
-    switch (actionId) {
-        case 0b100: console.log("test: 1 on; 2,3 off"); break;
-        default: console.log("no action");
+
+    // TODO: probably needs some refactoring
+    if ((actionId & indexF_mask) != 0) {
+        startIndexF();
     }
+    else {
+        endIndexF();
+    }
+
+    if ((actionId & middleF_mask) != 0) {
+        startMiddleF();
+    }
+    else {
+        endMiddleF();
+    }
+
+    if ((actionId & ringF_mask) != 0) {
+        startRingF();
+    }
+    else {
+        endRingF();
+    }
+
 }
 
-function mouseMoveDown() {
-    runCmd(`xte 'mousermove 0 ${mouseSpeeds[curSpeedInd]}'`);
+// Control variable for action dispatching
+let indexF_waitForEnd = false;
+let middleF_waitForEnd = false;
+let ringF_waitForEnd = false;
+
+//============================
+// Functions below handle what happens at the start and what at the end
+// of the touch on sensor
+//============================
+function startIndexF() {
+    if (indexF_waitForEnd) {
+        return;
+    }
+
+    if (isWindowSwitcherOn) {
+        toPrevWindow();
+    }
+    else {
+        mouseDown();
+    }
+    indexF_waitForEnd = true;
 }
 
-function mouseMoveUp() {
-    runCmd(`xte 'mousermove 0 -${mouseSpeeds[curSpeedInd]}'`);
+function endIndexF() {
+    if (!indexF_waitForEnd) {
+        return;
+    }
+
+    if (!isWindowSwitcherOn) {
+        mouseUp();
+    }
+    indexF_waitForEnd = false;
 }
 
-function mouseMoveLeft() {
-    runCmd(`xte 'mousermove -${mouseSpeeds[curSpeedInd]} 0'`);
+
+function startMiddleF() {
+    if (middleF_waitForEnd) {
+        return;
+    }
+    toggleWindowSwitcher();
+    middleF_waitForEnd = true;
 }
 
-function mouseMoveRight() {
-    runCmd(`xte 'mousermove ${mouseSpeeds[curSpeedInd]} 0'`);
+function endMiddleF() {
+    if (!middleF_waitForEnd) {
+        return;
+    }
+    toggleWindowSwitcher();
+    middleF_waitForEnd = false;
 }
 
-function mouseMoveUpLeft() {
-    runCmd(`xte 'mousermove -${mouseSpeeds[curSpeedInd]} -${mouseSpeeds[curSpeedInd]}'`);
+function startRingF() {
+    if (ringF_waitForEnd) {
+        return;
+    }
+
+    if (isWindowSwitcherOn) {
+        toNextWindow();
+    }
+    else {
+        changeMouseSpeed();
+    }
+    ringF_waitForEnd = true;
 }
 
-function mouseMoveUpRight() {
-    runCmd(`xte 'mousermove ${mouseSpeeds[curSpeedInd]} -${mouseSpeeds[curSpeedInd]}'`);
+function endRingF() {
+    if (!ringF_waitForEnd) {
+        return;
+    }
+    ringF_waitForEnd = false;
 }
 
-function mouseMoveDownLeft() {
-    runCmd(`xte 'mousermove -${mouseSpeeds[curSpeedInd]} ${mouseSpeeds[curSpeedInd]}'`);
-}
+//============================
+// Helper functions below actually 
+// execute mouse movements etc.
+//============================
 
-function mouseMoveDownRight() {
-    runCmd(`xte 'mousermove ${mouseSpeeds[curSpeedInd]} ${mouseSpeeds[curSpeedInd]}'`);
-}
-
+/**
+ * Turn on/off scrolling through the active windows.
+ */
 function toggleWindowSwitcher() {
     if (isWindowSwitcherOn) {
         isWindowSwitcherOn = false;
@@ -72,20 +142,52 @@ function toggleWindowSwitcher() {
     }
 }
 
+/**
+ * Move to the active window above the currently selected one.
+ */
+function toPrevWindow() {
+    runCmd("xte 'key Tab'");
+}
+
+/**
+ * Move to the active window below the currently selected one.
+ */
+function toNextWindow() {
+    runCmd("xte 'keydown Shift_L'");
+    runCmd("xte 'key Tab'");
+    runCmd("xte 'keyup Shift_L'");
+}
+
+/**
+ * Change the speed of the mouse.
+ * TODO: this needs to work together with python movement detection script
+ */
 function changeMouseSpeed() {
     curSpeedInd = (curSpeedInd + 1) % mouseSpeeds.length;
 }
 
+/**
+ * Left mouse button is pressed.
+ */
 function mouseDown() {
-    isMouseDown = true;
+    //isMouseDown = true;
     runCmd("xte 'mousedown 1'");
 }
 
+/**
+ * Left mouse button is released.
+ */
 function mouseUp() {
-    isMouseDown = false;
+    //isMouseDown = false;
     runCmd("xte 'mouseup 1'");
 }
 
+/**
+ * Executes the given command and prints the output of the command, if any,
+ * to stdout of this script.
+ * 
+ * @param {string} cmd Command we want to run.
+ */
 function runCmd(cmd) {
     exec(cmd, (err, stdout, stderr) => {
         if (err || stderr) {
